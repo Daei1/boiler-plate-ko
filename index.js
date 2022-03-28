ac3785478f6dde765d8fe1cf6240fd3fe85093c4
Daei1 <97814911+Daei1@users.noreply.github.com>
2022-03-28 22:02:44 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const {User} = require("./models/User");
+const config = require('./config/key');
+
+
+// aplication / x-www-form-urllencoded
+app.use(bodyParser.urlencoded({extended:true}));
+// aplication/json
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://daeilkim:abcd1234@boilerplate.b1dnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     
 }).then(()=> console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하신가요 ')
+})
+
+
+app.post('/register', (req,res)=> {
+ 
+  const user= new User(req.body);
+  console.log(req.body);
+  user.save((err,userInfo)=>{
+    if(err) return res.json({sucess:false, err})
+    return res.status(200).json({sucess:true})
+  })
 })
 
 app.listen(port, () => {
